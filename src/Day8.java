9d8be7134262e82fb9a07ef5f96dde80152a2130
Reymanth <reymanthkumard25ai@vetias.ac.in>
2026-06-13 12:35:12 +0530

    Record added

--- src/Day8.java
+++ src/Day8.java
@@ -1,1 +1,12 @@
 
+public class Day8 {
+    record CourseRecord(String department, String coursename){
+    }
+
+    static void main() {
+        CourseRecord courseRecord=new CourseRecord("~ AI ~","~ Bsc,CS(AI) ~");
+        System.out.println(courseRecord.department);
+        System.out.println(courseRecord.coursename);
+
+    }
+}
