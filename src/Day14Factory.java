2478b7f86c494de7f88644d0b25cc1af654b93fa
Reymanth <reymanthkumard25ai@vetias.ac.in>
2026-06-22 15:57:51 +0530

    Factory pattern added

---
+++ src/Day14Factory.java
@@ -0,0 +1,38 @@
+public class Day14Factory {
+    interface Transcation {
+        void send(String to, String message);
+    }
+
+    class CreditTranscation implements Transcation {
+        @Override
+        public void send(String to, String message) {
+            System.out.println("Amount Credited 800: " + message + ", to" + to);
+        }
+
+    }
+
+    class DebitTranscation implements Transcation {
+        @Override
+        public void send(String to, String message) {
+            System.out.println("Amount Debited 800: " + message + ", to" + to);
+        }
+    }
+
+    static Transcation getTranscation(Day14Factory factory, String type) {
+        return switch (type) {
+            case "Credit" -> factory.new CreditTranscation();
+            case "Debit" -> factory.new DebitTranscation();
+            default -> throw new IllegalArgumentException("Invalid account type:" + type);
+        };
+    }
+
+    static void main() {
+        Day14Factory factory = new Day14Factory();
+        String type = "Credit";
+        getTranscation(factory, type).send(" 123467798", "Amount 800 credited");
+    }
+}
+
+
+
+
